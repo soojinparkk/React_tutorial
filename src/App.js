0cc43c395fbf7d1c11de8b04b71e67b08217b049
sooooz <soojinpar1026@gmail.com>
2020-09-04 21:18:52 +0900

    use bind and setState in events

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode:'welcome',
       subject:{title:'WEB', sub:'World Wide Web'},
+      welcome:{title:'Welcome', desc:'Hello, React'},
       contents:[
         {id:1, title:'HTML',desc:'HTML is for information'},
         {id:2, title:'CSS',desc:'CSS is for design'},
@@ -21,15 +23,37 @@ class App extends Component {
   }
 
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} 
-        sub={this.state.subject.sub}>
-        </Subject>
+        {/* <Subject title={this.state.subject.title} 
+        sub={this.state.subject.sub}></Subject> */}
+        
+        <header>
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault();
+            // this.state.mode = 'welcome';
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
+
         <br></br>
+
         <Nav data={this.state.contents}></Nav>
         <br></br>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."/>
+
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
