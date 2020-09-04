0fc6cc33dd4d686ce24e29db13a52cfad4aec7e3
sooooz <soojinpar1026@gmail.com>
2020-09-04 22:38:52 +0900

    use events between components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode:'welcome',
+      selected_content_id:1,
       subject:{title:'WEB', sub:'World Wide Web'},
       welcome:{title:'Welcome', desc:'Hello, React'},
       contents:[
@@ -28,29 +29,39 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i++;
+      }
     }
 
     return (
       <div className="App">
-        {/* <Subject title={this.state.subject.title} 
-        sub={this.state.subject.sub}></Subject> */}
-        
-        <header>
-          <h1><a href="/" onClick={function(e){
-            e.preventDefault();
-            // this.state.mode = 'welcome';
-            this.setState({
-              mode:'welcome'
-            });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
+
+        <Subject title={this.state.subject.title} 
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+          this.setState({mode:'welcome'});
+        }.bind(this)}></Subject>
 
         <br></br>
 
-        <Nav data={this.state.contents}></Nav>
+        <Nav onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            // String으로 받으면 타입 변환
+            // Number(id)
+            selected_content_id:id
+          });
+        }.bind(this)}
+        data={this.state.contents}></Nav>
+
         <br></br>
 
         <Content title={_title} desc={_desc}></Content>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,14 @@ class Nav extends Component {
       var data = this.props.data;
       var i = 0;
       while (i < data.length) {
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+        lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/"+data[i].id}
+          // bind 두번째 인자부터 function의 첫번째 파라미터로 작성
+          onClick={function(id, e){
+            e.preventDefault();
+            this.props.onChangePage(id);
+          }.bind(this, data[i].id)}>{data[i].title}</a></li>)
         i++;
       }
 
